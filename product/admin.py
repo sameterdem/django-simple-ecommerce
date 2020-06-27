b0b9a0d97aee30bfefec41678fee939c0c20c197
Samet ERDEM <info@sameterdem.com>
2020-06-27 19:50:06 +0300

    Product model added

--- product/admin.py
+++ product/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Product
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'category']
+    list_filter = ['status', 'category']
+
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -18,5 +18,28 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+# Product model
+class Product(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır')
+    )
+    
+    category    = models.ForeignKey(Category, on_delete = models.CASCADE)
+    title       = models.CharField(max_length = 30)
+    keywords    = models.CharField(max_length = 255)
+    description = models.CharField(max_length = 255)
+    status      = models.CharField(max_length = 10, choices = STATUS)
+    image       = models.ImageField(blank = True, upload_to = 'images')
+    price       = models.FloatField()
+    amount      = models.IntegerField()
+    detail      = models.TextField()
+    create_at   = models.DateTimeField(auto_now_add = True)
+    update_at   = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.title
+    
     
     
